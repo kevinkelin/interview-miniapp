385458d4908c0f3054c2c38af2cca60d18167be2
yangyanxing <yangyanxing@360.cn>
2022-11-22 21:29:43 +0800

    初始化项目
    初始化项目，添加tabbar，添加vant

---
+++ miniprogram/custom-tab-bar/index.js
@@ -0,0 +1,47 @@
+// custom-tab-bar/index.js
+Component({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        active: 0,
+        list: [{
+                text: "首页",
+                icon: "home-o",
+                url: "/pages/index/index"
+            },
+            {
+                text: "收藏",
+                icon: "orders-o",
+                url: "/pages/collection/collection"
+            },
+            {
+                text: "我的",
+                icon: "user-o",
+                url: "/pages/myhome/myhome"
+            },
+        ]
+    },
+
+    methods: {
+        // 切换tabBar时的操作
+        onChange(e){
+          var i = e.detail;
+          wx.switchTab({
+            url: this.data.list[i].url,
+          })
+          this.setData({
+            active : i
+          })
+        },
+        init() {
+          const page = getCurrentPages().pop();
+          this.setData({
+         　  active: this.data.list.findIndex(item => item.url === `/${page.route}`)
+          });
+         }
+      },
+
+    
+    
+})
